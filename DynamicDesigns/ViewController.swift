42f4aa8f19dc36c70b1dadd2eb42803519e00248
İrem Sever <iremsever_@outlook.com>
2024-09-11 23:20:56 +0300

    Created

---
+++ DynamicDesigns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicDesigns
+//
+//  Created by İrem Sever on 11.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
