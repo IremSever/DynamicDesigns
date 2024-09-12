e92fc085f5f7d32dd0d90a0168bd05a36a00290c
İrem Sever <iremsever_@outlook.com>
2024-09-12 23:44:41 +0300

    Cropped and saved

--- DynamicDesigns/Home/HomeViewController.swift
+++ DynamicDesigns/Home/HomeViewController.swift
@@ -21,6 +21,7 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate & UI
     var path = UIBezierPath()
     var startPoint: CGPoint?
     var isDrawing = false
+    var panelSuccess: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +30,8 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate & UI
         let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
         imgSelected.addGestureRecognizer(longPressGesture)
         imgSelected.isUserInteractionEnabled = true
+        
+        createSavePanel()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -105,61 +108,101 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate & UI
     
     @IBAction func buttonRefresh(_ sender: Any) {
         imgSelected.image = UIImage(named: "upload")
-        imgDesigned.image = UIImage(named: "download")
-        UserDefaults.standard.removeObject(forKey: "url")
-        lblLink.text = ""
+              imgDesigned.image = UIImage(named: "download")
+              UserDefaults.standard.removeObject(forKey: "url")
+              lblLink.text = ""
+              
+              shapeLayer?.removeFromSuperlayer()
+              path.removeAllPoints()
+          
     }
     
     @IBAction func buttonCut(_ sender: Any) {
         if let croppedImage = cropImageToPath() {
-            // Kırpılan kısmı imgDesigned'da göster
             imgDesigned.image = croppedImage }
         
     }
     @IBAction func buttonSave(_ sender: Any) {
+        guard let imageToSave = imgDesigned.image, let imageData = imageToSave.pngData() else {
+            return
+        }
+        
+        let uniqueFileName = UUID().uuidString
+        let storageRef = storage.child("cropped_images/\(uniqueFileName).png")
+        
+        storageRef.putData(imageData, metadata: nil, completion: { _, error in
+            guard error == nil else {
+                print("*****Upload failed***** ", String(describing: error))
+                return
+            }
+            
+            storageRef.downloadURL(completion: { url, error in
+                guard let url = url, error == nil else {
+                    return
+                }
+                
+                
+                self.showSavePanel()
+                
+          
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    self.buttonRefresh(sender)
+                }
+            })
+        })
     }
     
+    // start drawing
     @objc func handleLongPress(_ gesture: UILongPressGestureRecognizer) {
         let location = gesture.location(in: imgSelected)
         
         if gesture.state == .began {
+            // start drawing
             startPoint = location
             isDrawing = true
             path = UIBezierPath()
             path.move(to: location)
             createShapeLayer()
         } else if gesture.state == .changed, isDrawing {
+            // drawing
             path.addLine(to: location)
             shapeLayer.path = path.cgPath
         } else if gesture.state == .ended {
             if isDrawing, let startPoint = startPoint {
+                // end point
                 path.addLine(to: startPoint)
                 shapeLayer.path = path.cgPath
                 isDrawing = false
             } else {
+                // if dont touch reset
                 shapeLayer.removeFromSuperlayer()
                 isDrawing = false
             }
         }
     }
     
+    // create shapelayer
     func createShapeLayer() {
-        shapeLayer?.removeFromSuperlayer()
+        shapeLayer?.removeFromSuperlayer()  // delete last
         shapeLayer = CAShapeLayer()
         shapeLayer.strokeColor = UIColor.yellow.cgColor
         shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.lineWidth = 10
-        shapeLayer.lineDashPattern = [10, 5]
+        shapeLayer.lineWidth = 8
+        shapeLayer.lineDashPattern = [8, 5]
         imgSelected.layer.addSublayer(shapeLayer)
     }
     
+    // cropped area looks white
     func cropImageToPath() -> UIImage? {
         guard let image = imgSelected.image else { return nil }
+        
         UIGraphicsBeginImageContextWithOptions(imgSelected.bounds.size, false, 0)
         let context = UIGraphicsGetCurrentContext()
         
+        // original image
         image.draw(in: imgSelected.bounds)
         
+        // cropped
         context?.addPath(path.cgPath)
         context?.setFillColor(UIColor.white.cgColor)
         context?.fillPath()
@@ -168,5 +211,29 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate & UI
         UIGraphicsEndImageContext()
         
         return croppedImage
+    }
+    
+    
+    func createSavePanel() {
+        panelSuccess = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        panelSuccess.center = self.view.center
+        panelSuccess.backgroundColor = UIColor.orange.withAlphaComponent(0.7)
+        panelSuccess.layer.cornerRadius = 20
+                
+        let label = UILabel(frame: panelSuccess.bounds)
+        label.text = "Saved!"
+        label.textAlignment = .center
+        label.textColor = .white
+        panelSuccess.addSubview(label)
+        
+        panelSuccess.isHidden = true
+        self.view.addSubview(panelSuccess)
+    }
+    
+    func showSavePanel() {
+        panelSuccess.isHidden = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.panelSuccess.isHidden = true
+        }
     }
 }
