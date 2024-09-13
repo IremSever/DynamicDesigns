8772ed4e2d3f803f83a53cb004cffbb795212ebb
İrem Sever <iremsever_@outlook.com>
2024-09-13 16:12:21 +0300

    Google Interstitial Ads

--- DynamicDesigns/ViewController.swift
+++ DynamicDesigns/ViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 import FirebaseRemoteConfig
 import GoogleMobileAds
 
-class ViewController: UIViewController, GADBannerViewDelegate {
-    var bannerView: GADBannerView!
+class ViewController: UIViewController, GADBannerViewDelegate, GADFullScreenContentDelegate {
+    var viewBanner: GADBannerView!
+    var viewInterstitial: GADInterstitialAd?
     private var viewBg1: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.cyan.withAlphaComponent(1)
@@ -30,20 +31,27 @@ class ViewController: UIViewController, GADBannerViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        
         view.addSubview(viewBg1)
         view.addSubview(viewBg2)
         view.addSubview(buttonStart)
         //google mobile ads
-        bannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
-        //addBannerViewToView(bannerView)
+        viewBanner = GADBannerView(adSize: GADPortraitAnchoredAdaptiveBannerAdSizeWithWidth(250))
+        viewBanner.adUnitID = "ca-app-pub-3940256099942544/2435281174"
+        viewBanner.rootViewController = self
+        viewBanner.load(GADRequest())
+        viewBanner.delegate = self
         
-        //bannerView.adSize = GADCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(viewWidth)
+        GADInterstitialAd.load(withAdUnitID: "ca-app-pub-3940256099942544/4411468910", request: GADRequest()) { ad, error in
+            if let error = error {
+                print("Failed to load interstitial ad: \(error.localizedDescription)")
+                return
+            }
+            self.viewInterstitial = ad
+            self.viewInterstitial?.fullScreenContentDelegate = self
+        }
         
-        bannerView.adUnitID = "ca-app-pub-3940256099942544/2435281174"
-        bannerView.rootViewController = self
-        bannerView.load(GADRequest())
-        bannerView.delegate = self
+        
         
         fetchValues()
     }
@@ -97,11 +105,11 @@ class ViewController: UIViewController, GADBannerViewDelegate {
     
     
     @IBAction func buttonStart(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController")
-        as! HomeViewController
-        navigationController?.pushViewController(homeViewController, animated:
-                                                    true)
+        if let viewInterstitial = viewInterstitial {
+            viewInterstitial.present(fromRootViewController: self)
+        } else {
+            goToHomeViewController()
+        }
     }
     
     func addBannerViewToView(_ bannerView: GADBannerView) {
@@ -126,12 +134,21 @@ class ViewController: UIViewController, GADBannerViewDelegate {
             ])
     }
     
+    // Reklam gösterimi tamamlandığında HomeViewController'a geçiş
+    func goToHomeViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        navigationController?.pushViewController(homeViewController, animated: true)
+    }
+    
+    // Interstitial reklam kapandığında çağrılan fonksiyon
+    func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+        goToHomeViewController()
+    }
 }
 
 extension ViewController {
     func bannerViewDidReceiveAd(_ bannerView: GADBannerView) {
-      addBannerViewToView(bannerView)
+        addBannerViewToView(bannerView)
     }
-   
 }
-
